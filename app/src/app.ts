a4ea3bbd651daeb99793f6fa566dae8c0d5b2bf8
MKriener <matthiaskriener@gmail.com>
2022-02-13 19:04:32 +0100

    decorators

--- app/src/app.ts
+++ app/src/app.ts
@@ -1,177 +1,124 @@
-type Admin = {
-    name: string;
-    privileges : string [];
-}
-
-type Employee = {
-    name : string;
-    startDate : Date;
-}
-
-// interface ElevatedEmployee implements Admin, Employee {}
-// both will result in same
-type ElevatedEmployee = Admin & Employee;
-
-const e1 : ElevatedEmployee = {
-    name: 'max',
-    startDate: new Date(),
-    privileges: ['enter'],
-}
-
-type Combinables = string | number;
-type Numeric = number | boolean;
-
-// intersection type
-type Universal = Combinables & Numeric;
-
-// function overload only overloads are possible use 'test' and 1 will be forbidden
-function addSomething(a: string, b: string): string
-function addSomething(a: number, b: string): string
-function addSomething(a: Combinables, b: Combinables) {
-    //type guard
-    if (typeof a === 'string' || typeof b === 'string') {
-        return a.toString() + b.toString();
-    }
-
-    return a + b;
-}
-
-
-const resultString = addSomething(1, 'K');
-resultString.split(' ');
-
-const fetchedUserData = {
-    id: 'a',
-    name: 'Joe',
-    job: {title: 'CEO', description: 'My own company'}
-}
-
-// checks if job exist on object and then if title exist
-console.log(fetchedUserData.job && fetchedUserData.job.title);
-//short way => named optional chaining
-console.log(fetchedUserData?.job?.title);
-
-
-const myUserInput = null;
-// will also except  null, false, ''
-const storedData = myUserInput ||'DEFAULT';
-
-// nullish  coalescing
-const storedDataOnlyOnNull = myUserInput ?? 'DEFAULT';
-
-
-type UnknownEmployee = Employee | Admin;
-
-function printEmployee(emp : UnknownEmployee) {
-    console.log('Name: ' + emp.name);
-    //if (typeof emp === 'Admin') {
-    if ('privileges' in emp) {
-      console.log('Privileges: ' + emp.privileges);
-    }
-
-    if ('startDate' in emp) {
-        console.log('StartDate: ' + emp.startDate);
+function Logger(logString: string) {
+    return function(constructor: Function) {
+        console.log(logString);
+        console.log(constructor);
     }
 }
 
-printEmployee({name: 'boy', privileges: ['enter']});
-printEmployee({name: 'boy', startDate: new Date('2020-01-01')});
+//will be executed on instantiation
+function WithTemplate(template: string, hookId :string) {
+    return function<T extends {new(...args: any[]): {name: string} }> (originalConstructor: T) {
+        console.log('Rendering template');
+        return class extends originalConstructor {
+            constructor(..._: any[]) {
+                super();
 
-
-class Car {
-    drive() {
-        console.log('Driving');
+                const hookElt = document.getElementById(hookId);
+                if (hookElt) {
+                    hookElt.innerHTML = template;
+                    hookElt.querySelector('h1')!.textContent = this.name;
+                }
+            }
+        }
     }
 }
 
-class Truck {
-    drive() {
-        console.log('Driving a truck');
-    }
+@Logger('LOGGING - PERSONONE')
+@WithTemplate('<h1>My Person Object</h1>', 'app')
+class PersonOne {
+    name = 'Bob';
 
-    loadCargo(amount: number) {
-        console.log('Load '+ amount);
-    }
-}
-
-type Vehicle = Car | Truck;
-
-function useVehicle(vehicle : Vehicle) {
-    vehicle.drive();
-    //if ('loadCargo' in vehicle) {
-    if (vehicle instanceof Truck) {
-        vehicle.loadCargo(100);
+    constructor() {
+        console.log('Creating person object');
     }
 }
 
-const v1 = new Car();
-const v2 = new Truck();
+const pers = new PersonOne();
 
-useVehicle(v1);
-useVehicle(v2);
+console.log(pers);
 
-
-//discriminated unions
-interface Bird {
-    typeOfAnimal: 'bird'; //will be autocompleted in switch case
-    flyingSpeed: number;
+function Log(target: any, propertyName: string | Symbol) {
+    console.log('Property decorator');
+    console.log(target, propertyName);
 }
 
-interface Horse {
-    typeOfAnimal: 'horse'; //will be autocompleted in switch case
-    runningSpeed: number;
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+    console.log('Accessor decorator!');
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+
 }
 
-type Animal = Bird | Horse;
+function Log3(target: any, name: string | Symbol, descriptor: PropertyDescriptor) {
+    console.log('Method decorator!');
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
 
-function moveAnimal(animal: Animal) {
-    let speed;
-    switch (animal.typeOfAnimal) {
-        case "bird":
-            speed = animal.flyingSpeed;
-            break;
-        case "horse":
-            speed = animal.runningSpeed;
-            break;
+function Log4(target: any, name: string | Symbol, position: number) {
+    console.log('Parameter decorator!');
+    console.log(target);
+    console.log(name);
+    console.log(position);
+}
+
+class Product {
+    @Log
+    title: string;
+    private _price: number;
+
+    @Log2
+    set price(val: number) {
+        if(val > 0) {
+            this._price = val;
+            return;
+        }
+
+        throw  Error('price hast obe graeter than 0');
     }
-    console.log('Moving speed of: '+ animal.typeOfAnimal + speed);
+
+    get price() {
+        return this._price;
+    }
+
+    constructor(t: string, p : number) {
+        this.title = t;
+        this._price = p ;
+    }
+
+    @Log3
+    getPriceWithTax(@Log4 tax : number) {
+        return this._price * (1 + tax);
+    }
 }
 
+const p1 = new Product('Book', 19);
+const p2 = new Product('Book 2', 29);
 
-moveAnimal({typeOfAnimal: "bird", flyingSpeed: 10});
-
-const paragraphQuery = document.querySelector('p');
-const paragraphId = document.getElementById('message-output');
-
-//default TS way
-const userInputElement = <HTMLInputElement>document.getElementById('user-input')!;
-userInputElement.value = 'Hi there !';
-
-//react way
-const userInputElementAgain = <HTMLInputElement>document.getElementById('user-input')! as HTMLInputElement;
-userInputElementAgain.value = 'Hi there with as';
-
-//or without exclamation mark //don't tell it will never be null
-const theUserInputElement = document.getElementById('user-input');
-
-if (theUserInputElement) {
-   (theUserInputElement as HTMLInputElement).value = 'Hi there because your never null!';
+function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+    const adjDescriptior: PropertyDescriptor = {
+        configurable: true,
+        enumerable: false,
+        get() {
+            return originalMethod.bind(this);
+        },
+    };
+    return adjDescriptior;
 }
 
-interface ErrorContainer { // {email: 'Not a valid email, username: 'Must start with a character' }
-    [prop: string] : string;
+class Printer {
+    message = 'This works!';
+
+    @Autobind
+    showMessage() {
+        console.log(this.message);
+    }
 }
 
-const errorBag1 : ErrorContainer= {
-    email: 'Not a valid email',
-    username : 'Must start with a character'
-}
+const p = new Printer();
 
-const errorBag2: ErrorContainer = {
-    1: 'has to be number',
-    email: 'invalid mail',
-}
-
-const errorBag3: ErrorContainer = {
-    username: 'invalid'
-}
+const button1 = document.querySelector('button')!;
+button1.addEventListener('click', p.showMessage);
